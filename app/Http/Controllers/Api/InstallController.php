f0b1eb5ef303531c22773f0adb497866f58dfaca
WangNingkai <1655586865@qq.com>
2019-12-31 14:11:16 +0800

    安装优化

---
+++ app/Http/Controllers/Api/InstallController.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * This file is part of the wangningkai/OLAINDEX.
+ * (c) wangningkai <i@ningkai.wang>
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace App\Http\Controllers\Api;
+
+
+use App\Http\Controllers\BaseController;
+
+class InstallController extends BaseController
+{
+    public function index()
+    {
+        // todo:自定义安装
+
+    }
+
+}
